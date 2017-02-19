b52959564519a8fd37fceec762ed06d8a819b045
lnwchok <sitthichok.punlabpho@gmail.com>
2017-02-19 14:28:00 +0700

    Create composer.sh

---
+++ composer.sh
@@ -0,0 +1,12 @@
+#Download composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+#Local install
+php composer.phar
+
+#Make it globally
+mv composer.phar /usr/local/bin/composer
+composer -V
